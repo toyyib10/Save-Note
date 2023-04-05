95e40923d8e6e48f5fbb292050dc86f975bc6f2b
Yekeen Toyyib <yekeentoyyib@gmail.com>
2023-04-05 16:54:23 +0100

    started working on database

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -1,5 +1,7 @@
+const userModel = require("../models/User.model")
+
 const signup = (req, res) => {
-  let {firstName,lastName,email,password} = req.body
+  let {firstName,lastName,email,phoneNumber,password} = req.body
 };
 
 module.exports = { signup };
