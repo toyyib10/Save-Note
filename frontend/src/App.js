0c97834e6a3fe7923a680d21a863653f0c2afbaa
Yekeen Toyyib <yekeentoyyib@gmail.com>
2023-04-15 23:18:22 +0100

    added component and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Auth from './routes/Auth';
+import Dashboard from './routes/Dashboard';
+import Notfound from './routes/Notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/auth/*' element={<Auth />} />
+        <Route path='/' element={<Navigate to="/auth/signin" />} />
+        <Route path='/dashboard/*' element={<Dashboard />} />
+        <Route path='/*' element={<Notfound/>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/routes/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Dashboard = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Dashboard
